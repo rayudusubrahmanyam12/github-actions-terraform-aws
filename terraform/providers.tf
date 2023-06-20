fcfdb82365eff4f85487deeda43afe37b1d95ac5
Subrahmanyam Rayudu <rayudusubrahmanyam12@gmail.com>
2023-06-20 10:16:50 +0530

    Test

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
   assume_role {
-    role_arn     = "arn:aws:iam::${var.account_id[terraform.workspace]}:role/${var.assume_role}"
+    role_arn     = "arn:aws:iam::${var.account_id}:role/${var.assume_role}"
     session_name = "example"
   }
 }
