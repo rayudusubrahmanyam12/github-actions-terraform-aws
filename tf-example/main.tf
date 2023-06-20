6204aed96639c4d84499e9f146155f80aa4fcb86
Subrahmanyam Rayudu <rayudusubrahmanyam12@gmail.com>
2023-06-20 08:09:35 +0530

    Test

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/*22.04-amd64-server-*"]
     }
 
     filter {
