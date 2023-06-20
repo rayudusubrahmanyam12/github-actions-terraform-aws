2dec4837e53a352172675eb5239609bbe6467c31
Subrahmanyam Rayudu <rayudusubrahmanyam12@gmail.com>
2023-06-20 08:12:15 +0530

    Test

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   key_name      = "saanvi-aws-2022"
 
