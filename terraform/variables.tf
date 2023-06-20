ab4955a62914c742bd49565d22c2f0cf508eeccd
Subrahmanyam Rayudu <rayudusubrahmanyam12@gmail.com>
2023-06-20 10:14:31 +0530

    Test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,12 @@ variable "acme_s3_logging_bucket" {
   description = "The s3 logging bucket"
   default = "acme-s3-access-bucket"
 }
+
+variable "account_id" {
+  default = "389028963485"
+  
+}
+
+variable "assume_role" {
+  default = "my-github-actions-role"
+}
